6a9981750448e50c9a961013920e8e588be15503
k8s-1 <176627561+k8s-1@users.noreply.github.com>
2025-11-26 19:14:13 +0100

    chore: update file(s): init.lua

--- init.lua
+++ init.lua
@@ -17,7 +17,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
---- lazy load plugins directory
+--- lazy load ./plugins/*.lua
 require("lazy").setup("plugins")
 
 
